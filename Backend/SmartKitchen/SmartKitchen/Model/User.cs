6a88a6dc20107487fdbac4bf4fb97f3508721215
Kbence9 <bence.kristian@gmail.com>
2024-05-02 15:39:35 +0200

    feat - add User.cs model

---
+++ Backend/SmartKitchen/SmartKitchen/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace SmartKitchen.Model;
+
+[Table("users")]
+public class User : IdentityUser
+{
+    public Refrigerator Refrigerator;
+}
