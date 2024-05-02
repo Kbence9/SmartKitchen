169630ed38f913543ea398d327b925d646e2aaee
Kbence9 <bence.kristian@gmail.com>
2024-05-02 15:35:40 +0200

    feat - Add Meal.cs

---
+++ Backend/SmartKitchen/SmartKitchen/Model/Meal.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartKitchen.Model;
+
+public class Meal
+{
+    public string Name;
+    public ICollection<Ingredient> Ingredients;
+    public Difficulty PreparationDifficulty;
+    public float ENERC_KCAL;
+    public float PROCNT;
+    public float FAT;
+    public float CHOCDF;
+    public float FIBTG;
+    public string Image;
+    public string Video;
+}
