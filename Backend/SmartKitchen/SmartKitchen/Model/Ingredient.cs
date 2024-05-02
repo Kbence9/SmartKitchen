23174d0727c8a1b46477828237b2625b487749b6
Kbence9 <bence.kristian@gmail.com>
2024-05-02 15:36:57 +0200

    feat - add Ingredient.cs model

---
+++ Backend/SmartKitchen/SmartKitchen/Model/Ingredient.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartKitchen.Model;
+
+public class Ingredient
+{
+    public string Name;
+    public float ENERC_KCAL;
+    public float PROCNT;
+    public float FAT;
+    public float CHOCDF;
+    public float FIBTG;
+    public string Image;
+}
