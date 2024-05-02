d8e4b0574f0ff3441c2249d687c5dabb0b386bbb
Kbence9 <bence.kristian@gmail.com>
2024-05-02 15:36:25 +0200

    feat - Add IIngredientRepository.cs interface

---
+++ Backend/SmartKitchen/SmartKitchen/Service/Repository/IIngredientRepository.cs
@@ -0,0 +1,12 @@
+﻿using SmartKitchen.Model;
+
+namespace SmartKitchen.Service.Repository;
+
+public interface IIngredientRepository
+{
+    Ingredient GetIngredient(string name);
+    void AddIngredient(Ingredient ingredient);
+    void DeleteIngredient(string name);
+    void UpdateIngredient(Ingredient ingredient);
+    
+}
