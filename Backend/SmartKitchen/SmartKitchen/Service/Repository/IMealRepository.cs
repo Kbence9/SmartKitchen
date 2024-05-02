6372a9af693292f0d425f56acff0afa68ccf9db2
Kbence9 <bence.kristian@gmail.com>
2024-05-02 15:36:38 +0200

    feat - Add IMealRepository.cs interface

---
+++ Backend/SmartKitchen/SmartKitchen/Service/Repository/IMealRepository.cs
@@ -0,0 +1,12 @@
+﻿using SmartKitchen.Model;
+
+namespace SmartKitchen.Service.Repository;
+
+public interface IMealRepository
+{
+    Meal GetMeal(string name);
+    void AddMeal(Meal meal);
+    void DeleteMeal(string name);
+    void UpdateMeal(Meal meal);
+    List<Meal> GetAllMeal();
+}
