aad809d4d4bc20b598ee673c5d8c6063469c50cc
Kbence9 <bence.kristian@gmail.com>
2024-05-17 10:49:57 +0200

    feat - add SmartKitchenContext.cs

---
+++ Backend/SmartKitchen/SmartKitchen/Data/SmartKitchenContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmartKitchen.Model;
+
+namespace SmartKitchen.Data;
+
+public class SmartKitchenContext : IdentityDbContext<User, IdentityRole, string>
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Meal> Meals { get; set; }
+
+    public SmartKitchenContext()
+    {
+    }
+
+    public SmartKitchenContext(DbContextOptions<SmartKitchenContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<User>()
+            .HasOne(u => u.Refrigerator)
+            .WithOne(r => r.user);
+    }
+
+}
