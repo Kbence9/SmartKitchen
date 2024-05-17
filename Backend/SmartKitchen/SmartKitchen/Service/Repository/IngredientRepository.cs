9f43c70e99eae493f0aaab5efaa34be7b89e1a5b
Kbence9 <bence.kristian@gmail.com>
2024-05-17 10:48:29 +0200

    feat - add IngredientRepository.cs

---
+++ Backend/SmartKitchen/SmartKitchen/Service/Repository/IngredientRepository.cs
@@ -0,0 +1,34 @@
+﻿using SmartKitchen.Data;
+using SmartKitchen.Model;
+
+namespace SmartKitchen.Service.Repository;
+
+public class IngredientRepository : IIngredientRepository
+{
+    private readonly SmartKitchenContext _smartKitchenContext;
+
+    public IngredientRepository(SmartKitchenContext smartKitchenContext)
+    {
+        _smartKitchenContext = smartKitchenContext;
+    }
+    public Ingredient GetIngredient(string name)
+    {
+        return _smartKitchenContext.Ingredients.Where(i => i.Name == name)
+            .FirstOrDefault();
+    }
+
+    public void AddIngredient(Ingredient ingredient)
+    {
+        _smartKitchenContext.Ingredients.Add(ingredient);
+    }
+
+    public void DeleteIngredient(Ingredient ingredient)
+    {
+        _smartKitchenContext.Ingredients.Remove(ingredient);
+    }
+
+    public void UpdateIngredient(Ingredient ingredient)
+    {
+        throw new NotImplementedException();
+    }
+}
