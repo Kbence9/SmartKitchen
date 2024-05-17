d52d6ddd93e680bd97758513af4a0987b882214e
Kbence9 <bence.kristian@gmail.com>
2024-05-17 10:47:03 +0200

    update - IngredientRepository.cs delete ingredient

--- Backend/SmartKitchen/SmartKitchen/Service/Repository/IIngredientRepository.cs
+++ Backend/SmartKitchen/SmartKitchen/Service/Repository/IIngredientRepository.cs
@@ -6,7 +6,7 @@ public interface IIngredientRepository
 {
     Ingredient GetIngredient(string name);
     void AddIngredient(Ingredient ingredient);
-    void DeleteIngredient(string name);
+    void DeleteIngredient(Ingredient ingredient);
     void UpdateIngredient(Ingredient ingredient);
     
 }
