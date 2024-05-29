c62f715a0c3bdf15e89891059e1025ca62022995
Kbence9 <bence.kristian@gmail.com>
2024-05-29 15:39:43 +0200

    update - add new constructor to Ingredient.cs

--- Backend/SmartKitchen/SmartKitchen/Model/Ingredient.cs
+++ Backend/SmartKitchen/SmartKitchen/Model/Ingredient.cs
@@ -8,5 +8,20 @@ public class Ingredient
     public float FAT;
     public float CHOCDF;
     public float FIBTG;
-    public string Image;
+    public string? Image;
+
+    public Ingredient()
+    {
+    }
+
+    public Ingredient(string name, float enercKcal, float procnt, float fat, float chocdf, float fibtg, string? image)
+    {
+        Name = name;
+        ENERC_KCAL = enercKcal;
+        PROCNT = procnt;
+        FAT = fat;
+        CHOCDF = chocdf;
+        FIBTG = fibtg;
+        Image = image;
+    }
 }
